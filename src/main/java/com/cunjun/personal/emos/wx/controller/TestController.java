e8cd80df1718bab4117fd0338f77b5713344aab3
Cunjun Wang <duckwcj@gmail.com>
2021-01-26 11:27:10 +0800

    配置Form类验证参数

--- src/main/java/com/cunjun/personal/emos/wx/controller/TestController.java
+++ src/main/java/com/cunjun/personal/emos/wx/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.cunjun.personal.emos.wx.controller;
 
 import com.cunjun.personal.emos.wx.common.util.ResultData;
+import com.cunjun.personal.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by CunjunWang on 2021/1/26.
@@ -17,8 +21,8 @@ public class TestController {
 
     @GetMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public ResultData sayHello() {
-        return ResultData.ok().put("message", "Hello World!");
+    public ResultData sayHello(@Valid @RequestBody TestSayHelloForm form) {
+        return ResultData.ok().put("message", "Hello, " + form.getName());
     }
 
 }
---
+++ src/main/java/com/cunjun/personal/emos/wx/controller/form/TestSayHelloForm.java
@@ -0,0 +1,22 @@
+package com.cunjun.personal.emos.wx.controller.form;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Created by CunjunWang on 2021/1/26.
+ */
+@Data
+@ApiModel
+public class TestSayHelloForm {
+
+    @NotBlank
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$")
+    @ApiModelProperty("姓名")
+    private String name;
+
+}
