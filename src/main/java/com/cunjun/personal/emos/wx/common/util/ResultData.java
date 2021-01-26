f2744e81b3f5fe7d89f554f90a8bf0939cb4f333
Cunjun Wang <duckwcj@gmail.com>
2021-01-26 11:01:03 +0800

    创建自定义异常类; 封装Web返回对象

---
+++ src/main/java/com/cunjun/personal/emos/wx/common/util/ResultData.java
@@ -0,0 +1,54 @@
+package com.cunjun.personal.emos.wx.common.util;
+
+import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by CunjunWang on 2021/1/26.
+ */
+public class ResultData extends HashMap<String, Object> {
+
+    public ResultData() {
+        put("code", HttpStatus.SC_OK);
+        put("msg", "success");
+    }
+
+    public static ResultData error() {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, "未知异常，请联系管理员");
+    }
+
+    public static ResultData error(String msg) {
+        return error(HttpStatus.SC_INTERNAL_SERVER_ERROR, msg);
+    }
+
+    public static ResultData error(int code, String msg) {
+        ResultData r = new ResultData();
+        r.put("code", code);
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static ResultData ok(String msg) {
+        ResultData r = new ResultData();
+        r.put("msg", msg);
+        return r;
+    }
+
+    public static ResultData ok(Map<String, Object> map) {
+        ResultData r = new ResultData();
+        r.putAll(map);
+        return r;
+    }
+
+    public static ResultData ok() {
+        return new ResultData();
+    }
+
+    public ResultData put(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+
+}
---
+++ src/main/java/com/cunjun/personal/emos/wx/exception/EmosException.java
@@ -0,0 +1,36 @@
+package com.cunjun.personal.emos.wx.exception;
+
+import lombok.Data;
+
+/**
+ * Created by CunjunWang on 2021/1/26.
+ */
+@Data
+public class EmosException extends RuntimeException {
+
+    private String msg;
+    private int code = 500;
+
+    public EmosException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
+
+    public EmosException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+    public EmosException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
+
+}
